58f063c2e155d06f7acd9dae42373cd73b70ccf2
julianest <julianest12@gmail.com>
2022-04-19 21:48:47 -0500

    se agrega server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonserver = require("json-server");
+const server = jsonserver.create();
+const router = jsonserver.router("./data.json");
+const middlewares = jsonserver.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 4050;
+server.listen(port,()=>{
+    console.log("Json server is runing");
+})
